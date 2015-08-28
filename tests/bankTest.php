0f32e764f86c470a57b4e6110882d7bb3df5d5fd
Neo <call@woola.net>
2015-08-28 23:02:52 +0800

    Added test Task

--- tests/bankTest.php
+++ tests/bankTest.php
@@ -19,6 +19,16 @@ class BankTest extends PHPUnit_Framework_TestCase
         $container->bank(function(){
             return (new Bank(new PinyinCurrency))->getCoin();
         });
+
+        // 测试拼音 的 储存
+
+
+        // 拼音的读取
+
+
+        // 更多的插件
+
+        
     }
 
 
