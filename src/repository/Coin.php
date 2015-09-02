11dd8e9b027d10d53d4133928f2b3fdbe824ff8c
Neo <call@woola.net>
2015-09-02 23:46:11 +0800

    Fix file Name

--- src/repository/Coin.php
+++ src/repository/Coin.php
@@ -2,25 +2,11 @@
 
 namespace Callwoola\SearchSuggest\repository;
 
-/**
- * Interface CoinInterface
- * @package Callwoola\SearchSuggest\repository
- */
-interface CoinInterface{
+class Coin implements CoinInterface{
 
 
-    /**
-     * 设置 coin的 key
-     *
-     * @return mixed
-     */
-    public function setName();
+    public function setName(){}
 
-    /**
-     * 设置子数据
-     *
-     * @return mixed
-     */
-    public function setCoin();
+    public function setCoin(){}
 
 }
