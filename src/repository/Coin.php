413fb5b9834652eb8f8ede9147bd6f309d323492
Neo <call@woola.net>
2015-09-02 23:46:38 +0800

    Added doc

--- src/repository/Coin.php
+++ src/repository/Coin.php
@@ -2,11 +2,21 @@
 
 namespace Callwoola\SearchSuggest\repository;
 
+/**
+ * Class Coin
+ * @package Callwoola\SearchSuggest\repository
+ */
 class Coin implements CoinInterface{
 
 
+    /**
+     *
+     */
     public function setName(){}
 
+    /**
+     *
+     */
     public function setCoin(){}
 
 }
